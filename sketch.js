54b13c0c97d61c41e0885b881c06ad5984e3e848
mjtheriot4 <mther16@lsu.edu>
2016-09-22 10:40:31 -0500

    started

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// Micah Theriot Copywright 2016
+
+var s;
+var t;
+var r;
+
+function setup (){
+  createCanvas(1000,600);
+  background(0);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  stroke(150);
+}
+function draw(){
+  translate(width/2,height/2);
+  rect(0,0,50,50);
+}
