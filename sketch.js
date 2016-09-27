d2c7c215ae6ac6d849461297458d74c8fd9ea6bb
mjtheriot4 <mther16@lsu.edu>
2016-09-27 09:55:48 -0500

    version3

--- sketch.js
+++ sketch.js
@@ -15,18 +15,22 @@ function draw(){
   blendMode(ADD);
   strokeWeight(w);
   stroke(255,0,0,a);
+  translate(10 * timeline,0);
   ellipse(0, height/2, i)
   stroke(0,0,255,a);
+  translate(-(20 * timeline),0);
   ellipse(width, height/2, i)
-  if (timeline <= 10){
+  if (timeline < 10){
     i += 20 + w * 3;
     w += .1;
     a -= 2.5;
+  }
     if (i > 2 * width) {
       i = 0;
-      w -=5;
+      w = .5;
       a = 200;
       timeline++;
     }
-  }
+
+
 }
